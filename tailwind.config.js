4441342d9ab996bdbec7a3bdb1373815499dd7cd
leohong <hongjiaenleo@gmail.com>
2024-09-16 15:39:07 +0800

    implemented basic login logic

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Corrected from 'color' to 'colors'
-        lightlightGreen: "#dbf0eb",
-        lightGreen: "#80c9b6",
-        darkGreen: "#00926d",
+        gold: "#996600",
+        yellow: "#cc9900",
+        "yellow-1": "#ffd400",
+        "yellow-2": "#faa61a",
+        blue: "#003366",
+        "blue-1": "#63cae1",
+        "blue-2": "#0074bc",
+        "blue-3": "#005587",
+        "blue-4": "#003366",
+        gray: "#cccccc",
+        "gray-1": "#8c8f90",
+        "green-1": "#a3cf62",
+        "green-2": "#00b08d",
+        "green-3": "#009a61",
+        "red-1": "#ed1b2f",
+        "red-2": "#7c2348",
       },
     },
   },
