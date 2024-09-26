281ace4bb6369b2634473ddcd983da73f682fe5b
Leo Hong <jhongan@connect.ust.hk>
2024-09-26 13:05:00 +0800

    add qr code in Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import CircularProgress from "@mui/material/CircularProgress";
+import { QRCodeSVG } from "qrcode.react";
 
 function Login() {
   const navigate = useNavigate();
@@ -53,74 +54,103 @@ function Login() {
       >
         <img
           src={`https://join.hkust.edu.hk/sites/default/files/2020-06/hkust.jpg`}
-          className={`w-full h-full object-cover absolute z-10 opacity-20 fade-in-20`}
+          className={`w-full h-full object-cover absolute z-10 opacity-30 fade-in-20`}
           alt={`image`}
         />
 
-        <div className="flex flex-col justify-center items-center p-10 bg-yellow rounded-[20px] z-20 w-[400px] h-[350px]">
+        <div className="flex flex-col bg-yellow rounded-[20px] z-20 w-[700px] h-[350px] absolute">
           {loading && (
-            <div
-              className={` w-[100px] h-[100px] flex justify-center items-center`}
-            >
-              <CircularProgress
-                size={`100%`}
-                sx={{ color: "rgba(0, 51, 102, 1)" }}
-              />
+            <div className={`w-full h-full items-center justify-center flex`}>
+              <div
+                className={`w-[100px] h-[100px] flex justify-center items-center`}
+              >
+                <CircularProgress
+                  size={`100%`}
+                  sx={{ color: "rgba(0, 51, 102, 1)" }}
+                />
+              </div>
             </div>
           )}
           {!loading && loginSuccess && (
-            <div className="absolute text-blue font-bold text-2xl text-center">
-              Login Success <br /> Redirecting...
+            <div className={`w-full h-full items-center justify-center flex`}>
+              <div className="absolute text-blue font-bold text-2xl text-center">
+                Login Success <br /> Redirecting...
+              </div>
             </div>
           )}
           {!loading && !loginSuccess && (
             <>
-              <div className="w-[280px] font-bold text-[37px] text-center text-blue select-none">
-                Virtual Window
+              <div className="w-full font-bold text-[28px] mt-[50px] text-center text-blue select-none absolute mb-auto">
+                Virtual Window for Workplace Well-being
               </div>
-              <div className={`w-[280px] text-[20px]`}>
-                <form onSubmit={handleSubmit}>
-                  <div className="relative mt-[20px] w-full">
-                    <input
-                      type="text"
-                      name="username"
-                      placeholder="Username"
-                      className="login-input-style"
-                    />
-                  </div>
-                  <div className="relative focus-within:text-blue-2 text-blue mt-[8px]">
-                    <input
-                      type={showPassword ? "text" : "password"}
-                      name="password"
-                      placeholder="Password"
-                      className="login-input-style pr-10"
-                    />
-                    <span
-                      className="absolute right-2 top-4 cursor-pointer"
-                      onMouseDown={(e) => {
-                        e.preventDefault();
-                        setShowPassword(!showPassword);
-                      }}
+              <div
+                className={`w-full absolute h-fit flex item-center justify-center mt-[100px]`}
+              >
+                <div className={`w-[280px] text-[20px] mr-[55px]`}>
+                  <form onSubmit={handleSubmit}>
+                    <div className="relative mt-[20px] w-full">
+                      <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        className="login-input-style"
+                      />
+                    </div>
+                    <div className="relative focus-within:text-blue-2 text-blue mt-[8px]">
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        placeholder="Password"
+                        className="login-input-style pr-10"
+                      />
+                      <span
+                        className="absolute right-2 top-4 cursor-pointer"
+                        onMouseDown={(e) => {
+                          e.preventDefault();
+                          setShowPassword(!showPassword);
+                        }}
+                      >
+                        {showPassword ? (
+                          <VisibilityOutlinedIcon />
+                        ) : (
+                          <VisibilityOffOutlinedIcon />
+                        )}
+                      </span>
+                    </div>
+                    <div
+                      className={`text-red-2 font-bold text-xs ${loginFailed ? `` : `invisible`}`}
                     >
-                      {showPassword ? (
-                        <VisibilityOutlinedIcon />
-                      ) : (
-                        <VisibilityOffOutlinedIcon />
-                      )}
-                    </span>
-                  </div>
+                      Invalid Login Credential
+                    </div>
+                    <button
+                      type="submit"
+                      className="bg-blue w-full mt-[8px] text-white p-1 rounded-3xl hover:bg-blue-2 uppercase "
+                    >
+                      login
+                    </button>
+                  </form>
+                </div>
+                <div
+                  className={`w-[210px] pl-[20px] border-blue border-dashed border-l-2 mt-[20px]`}
+                >
                   <div
-                    className={`text-red-2 font-bold text-xs ${loginFailed ? `` : `invisible`}`}
+                    className={`items-center justify-center flex flex-col mt-[10px]`}
                   >
-                    Invalid Login Credential
+                    <div>
+                      <QRCodeSVG
+                        value={`Virtual Window for Workplace Well-being`}
+                        size={120}
+                        bgColor="transparent"
+                        fgColor="#003366"
+                      />
+                    </div>
+                    <div
+                      className={`text-blue-3 text-[14px] text-center mt-2 leading-4`}
+                    >
+                      Scan this with your control app to sign in
+                    </div>
                   </div>
-                  <button
-                    type="submit"
-                    className="bg-blue w-full mt-[8px] text-white p-1 rounded-3xl hover:bg-blue-2 uppercase "
-                  >
-                    login
-                  </button>
-                </form>
+                </div>
               </div>
             </>
           )}
